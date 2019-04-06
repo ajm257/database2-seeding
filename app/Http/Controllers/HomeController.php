c429e99458ee66a37dcde4ccd155bfba24bed7c5
ajm <ajm257@njit.edu>
2019-04-06 11:28:29 -0400

    Laravel auth setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function profile()
+    {
+        $users = User::all();
+
+        return view('profile', ['users' => $users]);
+    }
+}
