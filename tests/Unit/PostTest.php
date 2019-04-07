55a6101828ce2d0265364d184755614ce67c2f84
ajm <ajm257@njit.edu>
2019-04-06 22:59:04 -0400

    Post Test 1) to find posts in random order and take the first post found to and 2) to count records and make sure record count is an integer

---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testPost()
+    {
+        $post = Post::InRandomOrder()->first();
+        $this->assertInternalType('string', $post->title);
+    }
+
+    public function testUserCount()
+    {
+        $posts = Post::All();
+        $postcount = $posts->count();
+        $this->assertInternalType('int', $postcount);
+    }
+}
